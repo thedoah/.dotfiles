35972d964aedbef9524b4108e3300b1278f90b0d
dogukan.ahucalar <dogukan.ahucalar@dk.hepsiburada.com>
2024-02-15 12:16:16 +0300

    lsp configuration added, autocompletion and snippets added, snippets added for nvim and lsp snippets

--- lua/thedoah/lazy/treesitter.lua
+++ lua/thedoah/lazy/treesitter.lua
@@ -7,6 +7,7 @@ return {
 
             configs.setup({
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "javascript", "html" },
+                auto_install = true,
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },  
